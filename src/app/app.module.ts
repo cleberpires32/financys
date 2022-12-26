c8b7f06932c12f19c4276f3953b80d54352c648f
cleberpires32 <cleberpires269@gmail.com>
2022-12-26 17:36:43 -0300

    intercepting http reuests with angular-in-memory-we-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { Categoria } from './pages/shared/categoria.model';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriaFormComponent } from './pages/categorias/categoria-form/categoria-form.component';
 import { CategoriaListComponent } from './pages/categorias/categoria-list/categoria-list.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataBase } from './in-memory-database';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import { CategoriaListComponent } from './pages/categorias/categoria-list/catego
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
   bootstrap: [AppComponent]
